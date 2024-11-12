a27dbcbf538b025f6576e16faff5e78ce8aaf6aa
Alexandre Le Moëligou <alexlemoeligou@gmail.com>
2024-11-11 22:07:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export const config = {
   domainSource: "https://dbca59a5-fca8-4527-9d3a-2871dbf3822c.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/recipe/[^/]+",
-          metaDataEndpoint: "https://x1ep-0nkn-l9fv.n2.xano.io/api:biMU4Ny2/recipes/get_single/{recipes_id}"
+          pattern: "/r/[^/]+",
+          metaDataEndpoint: "https://x1ep-0nkn-l9fv.n2.xano.io/api:biMU4Ny2/recipes/get_single/{recipes_slug}"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
