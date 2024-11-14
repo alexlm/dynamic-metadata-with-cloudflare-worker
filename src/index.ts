f0ab511f0cde3839caf9ba04047f680ece702290
Alexandre Le Moëligou <alexlemoeligou@gmail.com>
2024-11-14 10:15:21 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -214,7 +214,15 @@ class CustomHeaderHandler {
         console.log('Removing noindex tag');
         element.remove();
       }
-	    
+
+      // Replace favicon URL
+	if (element.tagName == "link") {
+	  const rel = element.getAttribute("rel");
+	  if (rel === "icon" || rel === "shortcut icon") {
+	    console.log('Replacing favicon URL');
+	    element.setAttribute("href", `${config.domainSource}/favicon.ico?_wwcv=150`);
+	  }
+	}   
     }
   }
 }
