6cbf4b69e526833e7f205aa453caa98c3283e8bb
Alexandre Le Moëligou <alexlemoeligou@gmail.com>
2025-06-25 08:13:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export const config = {
   domainSource: "https://dbca59a5-fca8-4527-9d3a-2871dbf3822c.weweb-preview.io", // Your WeWeb app preview link
-  //domainSource: "https://40f5cbb5-91ba-4dda-9a39-9d08d52d3b08.weweb-preview.io",
   patterns: [
       {
           pattern: "/r/[^/]+",
